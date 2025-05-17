070a77ca96a65bb9477c813b166ddf7e11d53b0b
kamycoding <kamyar.zamanfar@gmail.com>
2025-05-17 23:48:16 +0200

    Initial commit : add custom scroll project files

---
+++ app.js
@@ -0,0 +1,8 @@
+let contentBtn = document.querySelector(".content__btn");
+let content = document.querySelector(".content");
+
+contentBtn.addEventListener("click", function () {
+  content.classList.toggle("content--open");
+});
+
+
